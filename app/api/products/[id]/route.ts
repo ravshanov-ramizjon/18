265fef89c11eb472e05ab4ce08672e806565d101
ravshanov-ramizjon <ravsanovramiz@gmail.com>
2025-05-09 16:08:25 +0500

    fix error 4

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,13 +1,17 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import type { NextRequest } from "next/server";
 
-type Params = { params: { id: string } };
-
-export async function DELETE(req: NextRequest, { params }: Params) {
+// DELETE /api/products/[id]
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const id = parseInt(params.id);
 
-  if (isNaN(id)) return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+  if (isNaN(id)) {
+    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+  }
 
   await prisma.product.delete({
     where: { id },
@@ -16,11 +20,17 @@ export async function DELETE(req: NextRequest, { params }: Params) {
   return NextResponse.json({ success: true });
 }
 
-export async function PUT(req: Request, { params }: Params) {
+// PUT /api/products/[id]
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const id = parseInt(params.id);
   const data = await req.json();
 
-  if (isNaN(id)) return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+  if (isNaN(id)) {
+    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+  }
 
   const updatedProduct = await prisma.product.update({
     where: { id },
